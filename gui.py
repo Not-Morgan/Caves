1d0b9cbb5f881edfe252198b839858c8c58695c7
Josh <pgry@null.net>
2018-04-30 09:56:07 -0700

    no idea what is happening

---
+++ gui.py
@@ -0,0 +1,23 @@
+# all the definitions for stuff
+import pygame
+
+# define colours of the buttons
+black = (0, 0, 0)
+
+mouse = pygame.mouse.get_pos()
+font = pygame.font.SysFont(None, 17)
+
+
+def center_text(screen, text, x, y):
+    text = font.render(str(text), True, black)
+    screen.blit(text, (x - text.get_rect().width / 2, y))
+
+
+def display_buttons(screen, button_pos, text, colour, dim_colour):  # button_pos takes x, y, len x, len y
+    if button_pos[0] + button_pos[2] > mouse[0] > button_pos[0] and button_pos[1] + button_pos[3] > mouse[1] > \
+            button_pos[1]:  # if mouse is inside the button
+        pygame.draw.rect(screen, colour, (button_pos[0], button_pos[1], button_pos[2], button_pos[3]))  # change colour
+        center_text(screen, text, 300, button_pos[1] + 17)
+    else:
+        pygame.draw.rect(screen, dim_colour, (button_pos[0], button_pos[1], button_pos[2], button_pos[3]))
+        center_text(screen, text, 300, button_pos[1] + 17)
--- menu.py
+++ menu.py
@@ -1,3 +1,28 @@
+import pygame
 import game
+from gui import *
+
+# define colours of the buttons
+dim_red = (200, 0, 0)
+dim_green = (0, 200, 0)
+dim_blue = (136, 206, 250)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 191, 255)
+black = (0, 0, 0)
+
+gameDisplay = game.game_mgr.gameDisplay
+pygame.display.set_caption('Welcome')
+started = False
+
+
+def drawmenu():
+    display_buttons(gameDisplay, [100, 100, 10, 10], 'Start', green, dim_green)
+
+
+while not started:
+    drawmenu()
+    pygame.display.update()
+    game.game_mgr.clock.tick(60)
 
 game.start()
