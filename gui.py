77ccb7d1f651b01fdeb662c42348de6e8a2ce16f
Josh <pgry@null.net>
2018-05-03 00:05:45 -0700

    its good

--- gui.py
+++ gui.py
@@ -9,22 +9,65 @@ black = (0, 0, 0)
 pygame.font.init()
 
 
-def center_text(screen, text, x, y, font_size):
+def center_text(screen, text, x, y, font_size, colour):
     font = pygame.font.SysFont(None, font_size)
-    text = font.render(str(text), True, black)
+    text = font.render(str(text), True, colour)
     screen.blit(text, (x - text.get_rect().width / 2, y))
 
 
 def display_buttons(screen, button_pos, text, colour, dim_colour):  # button_pos takes x, y, len x, len y
+
     mouse = pygame.mouse.get_pos()
+
+    def buttonClicked():
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                return True
+
     if button_pos[0] + button_pos[2] > mouse[0] > button_pos[0] and button_pos[1] + button_pos[3] > mouse[1] > button_pos[1]:  # if mouse is inside the button
         pygame.draw.rect(screen, colour, (button_pos[0], button_pos[1], button_pos[2], button_pos[3]))  # change colour
-        center_text(screen, text, button_pos[0] + button_pos[2]/2, button_pos[1] + 17, 17)
+        center_text(screen, text, button_pos[0] + button_pos[2]/2, button_pos[1] + 17, 17, black)
         print(mouse, "in button")
-        # for event in pygame
-        # if pygame.mouse.get_pressed()[]
-        return text
+        if buttonClicked():
+            print("I am clicked!")
+            return str(text)
     else:
         pygame.draw.rect(screen, dim_colour, (button_pos[0], button_pos[1], button_pos[2], button_pos[3]))
-        center_text(screen, text, button_pos[0] + button_pos[2]/2, button_pos[1] + 17, 17)
+        center_text(screen, text, button_pos[0] + button_pos[2]/2, button_pos[1] + 17, 17, black)
         print(mouse, "not in button")
+
+"""
+def button_pressed(score, price1, price2, price3, price4):
+        
+        button1_pos = [515, 100, 75, 50]
+        
+
+        mouse = pygame.mouse.get_pos()
+
+        #define the function of if a mouse is clicked in the boundary of the buttons
+        
+        def buttonClicked(button_pos):
+                if button_pos[0] + button_pos[2] > mouse[0] > button_pos[0] and button_pos[1] + button_pos[3] > mouse[1] > button_pos[1]: #if the mouse is in the boundaries of the button
+                    for event in pygame.event.get():
+                        if event.type == pygame.MOUSEBUTTONDOWN: #and the mouse is clicked
+                            return True
+                
+
+        #checks if the buttons are clicked and returns them to the main script
+
+        if buttonClicked(button1_pos) and score >= price1:
+                return "Apple"
+        if buttonClicked(button2_pos) and score >= price2:
+                return "Snake"
+        if buttonClicked(button3_pos) and score >= price3:
+                return "Snake2"
+        if buttonClicked(button4_pos) and score >= price4:
+                return "Snake3"
+        if buttonClicked(buttoncheat_pos):
+                return "Snake4"
+        if buttonClicked(buttoncredits_pos):
+                webbrowser.open("https://github.com/Not-Morgan/Caves")
+        
+        else:
+                return "None"
+    """
--- menu.py
+++ menu.py
@@ -13,26 +13,42 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 191, 255)
 black = (0, 0, 0)
+white = (255, 255, 255)
 
 gameDisplay = game.game_mgr.gameDisplay
 pygame.display.set_caption('Welcome')
 started = False
 
+def initialization():
+    pygame.init()
+    # set sound
+    intro_sound = pygame.mixer.Sound("sounds/intro.ogg")
+    gameplay_sound = pygame.mixer.Sound("sounds/gameplay.ogg")
+    button_click = pygame.mixer.Sound("sounds/button.ogg")
+    img = pygame.image.load('caves.jpg')
+    gameDisplay.blit(img, (0,0))
+    intro_sound.set_volume(1.0)                                    
+    intro_sound.play (-1,0,0)
+
+
 def draw_menu():
     gameDisplay.fill(game.white)
+    img = pygame.image.load('caves.jpg')
+    gameDisplay.blit(img, (0,0))
     font = pygame.font.SysFont(None, 17)
-    text = font.render('Welcome to Caves !', False, (0, 0, 0))
+    text = font.render('Welcome to Caves !', False, white)
     gameDisplay.blit(text, (10, 10))
 
     if (display_buttons(gameDisplay, [460, 180, 80, 40], 'Start', green, dim_green)) == 'Start':
-        started = False
+        started = True
 
     # mouse = pygame.mouse.get_pos()
-    center_text(gameDisplay, "Click here to Start", 500, 150, 25)
+    center_text(gameDisplay, "Click here to Start", 500, 150, 25, white)
+    center_text(gameDisplay, "Caves!", 500, 100, 50, white)
 
-    center_text(gameDisplay, "Caves!", 500, 100, 50)
 
 
+initialization()
 while not started:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
